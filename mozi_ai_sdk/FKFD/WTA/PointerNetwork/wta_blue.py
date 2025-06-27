9e742ff17b227735d3253d91fbbd3bc5c012004e
Bella <2323688459@qq.com>
2025-06-27 11:34:40 +0800

    # multi to one blue_model

--- mozi_ai_sdk/FKFD/WTA/PointerNetwork/wta_blue.py
+++ mozi_ai_sdk/FKFD/WTA/PointerNetwork/wta_blue.py
@@ -30,7 +30,7 @@ def wta_update_mask(mask, chosen_idx, num_weapon=20, num_target=20):
     for i in range(chosen_idx.size()[0]):
         target_array.append(list(range(target_index[i], end_c[i], num_target)))
     target_mask = torch.tensor(target_array).to(device)
-    mask.scatter_(1, weapon_mask, 0)
+    # mask.scatter_(1, weapon_mask, 0)
     mask.scatter_(1, target_mask, 0)
     return mask
 
@@ -151,7 +151,6 @@ class ProWTADataset(Dataset):
         self.dataset2 = torch.cat(
             (self.blue_type1, self.blue_speed1, self.blue_azimuth1, self.blue_range1, self.blue_longitude1,
              self.blue_latitude1), dim=1)
-
 
     def init_red_input(self, num_blue):
         self.red_type_wta = self.red_type
